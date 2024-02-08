dca18df9c0c6f4e57f55ce144cdc03f138affaf7
Simranjit Singh <50419132+SimranPabla@users.noreply.github.com>
2024-02-08 11:59:22 -0700

    Update Packet_Sniffer.py

--- Packet_Sniffer.py
+++ Packet_Sniffer.py
@@ -2,25 +2,41 @@ import scapy.all as scapy
 import sys
 import click
 @click.command()
-@click.argument('interface', type=str )
+@click.argument('interface', type=str)
 @click.argument('mode', type=click.Choice(['summary', 'detail']))
 @click.option('-f', '--filter', type=str, help='BPF filter expression')
+@click.option('-o', '--output',type=str, help="Output file name")
+def sniff_packets(interface, mode, filter=None, output=None):
+    packets = []
 
-def sniff_packets(interface, mode, filter=None):
-        try:
-                if mode == "summary":
-                        scapy.sniff(iface=interface, store=False, filter=filter, prn=process_packet_summary)
-                elif mode == "detail":
-                        scapy.sniff(iface=interface, store=False, filter=filter, prn=process_packet_detail)
-        except scapy.Scapy_Exception as e:
-                click.echo(f"An error occurred: {e}")
-                sys.exit(1)
-def process_packet_summary(packet):
-        click.echo(packet.summary())
+    def process_packet_summary(packet):
+        packets.append(packet.summary())
+        if output != "":
+            click.echo(packet.summary())
 
-def process_packet_detail(packet):
-        click.echo(packet.summary())
-        click.echo(packet.show())
+    def process_packet_detail(packet):
+        packets.append(packet.summary())
+        packets.append(str(packet.show(dump=True)))
+        if output != "":
+            click.echo(packet.summary())
+            click.echo(packet.show())
+    try:
+        if mode == "summary":
+            scapy.sniff(iface=interface, store=False, filter=filter, prn=process_packet_summary)
+        elif mode == "detail":
+            scapy.sniff(iface=interface, store=False, filter=filter, prn=process_packet_detail)
+
+        if output:
+            with open(output, 'w', encoding="utf-8") as f:
+                for packet in packets:
+                    f.write(packet + '\n')
+        else:
+            for packet in packets:
+                click.echo(packet)
+
+    except scapy.Scapy_Exception as e:
+        click.echo(f"An error occurred: {e}")
+        sys.exit(1)
 
 if __name__ == "__main__":
     try:
